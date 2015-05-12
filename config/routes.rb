87cca91279b1b544a3c9c2f008064a2849288c54
mhanna1 <mhanna500@gmail.com>
2015-05-12 16:46:21 -0400

    fixing multi hosting diffs

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   resources :password_resets,     only: [:new, :create, :edit, :update]
 end
 
+  #
   # The priority is used upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
