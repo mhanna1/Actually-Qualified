0a103ae51b5441c5fb05cf3f70ce9dacf91655f4
Mark Hanna <mhanna500@gmail.com>
2015-05-16 17:10:58 +0000

    after working

--- Gemfile
+++ Gemfile
@@ -1,9 +1,10 @@
 source 'https://rubygems.org'
 
 gem 'rails',        '4.2.0'
+gem 'json',         '1.8.2'
 gem 'mysql2',       '0.3.18'
 gem 'aws-sdk',      '~> 2'
-gem 'puma',        '2.11.1'
+# gem 'puma',         '2.11.1'
 gem 'bcrypt',       '3.1.10'
 gem 'faker',        '1.4.2'
 gem 'rspec',	    '>= 3.0'
@@ -19,7 +20,7 @@ gem 'jbuilder',     '2.2.3'
 gem 'sdoc',         '0.4.0', group: :doc
 
 group :development, :test do
-  gem 'byebug',      '3.4.0'
+# gem 'byebug'     
   gem 'web-console', '2.0.0.beta3'
   gem 'spring',      '1.1.3'
 end
--- config/puma.rb
+++ config/puma.rb
@@ -1,20 +1,54 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['MAX_THREADS'] || 5)
-threads threads_count, threads_count
+#-----  oomented out by MWH 
 
-preload_app!
+#----- workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+#----- threads_count = Integer(ENV['MAX_THREADS'] || 5)
+#----- threads threads_count, threads_count
 
-rackup      DefaultRackup
-port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'development'
+#----- preload_app!
 
-on_worker_boot do
+#----- rackup      DefaultRackup
+#----- port        ENV['PORT']     || 3000
+#----- environment ENV['RACK_ENV'] || 'development'
+
+#----- on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/
   # deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   #
   
-  ActiveRecord::Base.establish_connection
+#-----  ActiveRecord::Base.establish_connection
 
+#-----  end
+
+# Change to match your CPU core count
+workers Integer(ENV['WEB_CONCURRENCY'] || 1)
+
+# Min and Max threads per worker
+threads_count = Integer(ENV['MAX_THREADS'] || 6)
+threads threads_count, threads_count
+# threads 1, 6
+
+app_dir = File.expand_path("../..", __FILE__)
+shared_dir = "#{app_dir}/shared"
+
+# Default to production
+rails_env = ENV['RAILS_ENV'] || "production"
+environment rails_env
+
+# Set up socket location
+bind "unix://#{shared_dir}/sockets/puma.sock"
+
+# Logging
+stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+
+# Set master PID and state locations
+pidfile "#{shared_dir}/pids/puma.pid"
+state_path "#{shared_dir}/pids/puma.state"
+activate_control_app
+
+on_worker_boot do
+  require "active_record"
+  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
 end
 
