f3e58c9f68eb4ecf8c8a470dd88f52bcd21b47bb
mhanna1 <mhanna500@gmail.com>
2015-05-20 21:53:10 -0400

    working on authentication

--- app/assets/javascripts/timeclock.js
+++ app/assets/javascripts/timeclock.js
@@ -1,13 +1,6 @@
 
 <script type="text/javascript">
 
-    function pgClock() {
-        var sTime=(new Date()).toString();
-        document.getElementById('clockid2').innerHTML =
-            sTime.substring(0,3+sTime.lastIndexOf(':'));
-        setTimeout('pgClock()',333);
-    }
-
 
 window.onload=function(){
         pgClock();
