c64ef7d8a1825db579e38813989a4d1e0c6ba386
mhanna1 <mhanna500@gmail.com>
2015-06-04 00:15:36 -0400

    Added Professional Resume 6-4-2015

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -25,10 +25,10 @@ class StaticPagesController < ApplicationController
 
 
   def resume
-    unless logged_in?
-      flash[:info] = "Please login to view the resume page"
-      redirect_to login_url
-    end
+ #   unless logged_in?
+ #     flash[:info] = "Please login to view the resume page"
+ #     redirect_to login_url
+ #   end
   end
 
   def about
