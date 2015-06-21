7b36d46960da2257b0b4ccd012db3c484384df7a
mhanna1 <mhanna500@gmail.com>
2015-06-21 18:31:14 -0400

    Added robots.txt

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -3,23 +3,24 @@ require File.join(File.dirname(__FILE__), '..', 'test_helper.rb')
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
+
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", mission_path
-#   assert_select "a[href=?]", code_path
-#   assert_select "a[href=?]", languages_path
-#   assert_select "a[href=?]", simular_path
-#   assert_select "a[href=?]", fsd_path
-    assert_select "a[href=?]", aws_path
-#   assert_select "a[href=?]", resume_path
-#   assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", contact_path
-#   get signup_path
-#   assert_select "title", full_title("Sign up")
-    assert_select "a[href=?]", signup_path
+    assert_select "a", :href => root_path
+    assert_select "a", :href => mission_path
+    assert_select "a", :href => contact_path
+    assert_select "a", :href => aws_path
+    assert_select "a", :href => signup_path
+    assert_select "a", :href => languages_path
+
+    assert_select "a", :href => simular_path
+    assert_select "a", :href => fsd_path
+    assert_select "a", :href => resume_path
+    assert_select "a", :href => about_path
+
+    get signup_path
+    assert_select "title", "Sign up"
+
   end
-  # test "the truth" do
-  #   assert true
-  # end
+
 end
